e9f2dd220517344baea5f83444c50f71627b1dec
Yangyang Liu <yal103@ucsd.edu>
2026-05-14 22:26:07 -0700

    add bike lanes

--- map.js
+++ map.js
@@ -17,3 +17,37 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+const bikeLaneStyle = {
+  type: "line",
+  paint: {
+    "line-color": "green",
+    "line-width": 3,
+    "line-opacity": 0.4,
+  },
+};
+
+map.on("load", async () => {
+  // Boston bike lanes
+  map.addSource("boston_route", {
+    type: "geojson",
+    data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson",
+  });
+
+  map.addLayer({
+    ...bikeLaneStyle,
+    id: "boston-bike-lanes",
+    source: "boston_route",
+  });
+
+  map.addSource("cambridge_route", {
+    type: "geojson",
+    data: "https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson",
+  });
+
+  map.addLayer({
+    ...bikeLaneStyle,
+    id: "cambridge-bike-lanes",
+    source: "cambridge_route",
+  });
+});
